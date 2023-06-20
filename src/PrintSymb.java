b458d4d8a9c524a329a44482968dcd97cbdeeba2
MihailSHrus <97229510+MihailSHrus@users.noreply.github.com>
2023-06-20 17:02:40 +0400

    Initial commit

---
+++ src/PrintSymb.java
@@ -0,0 +1,9 @@
+public class PrintSymb {
+    public static void main(String[] args) {
+        char symb = 'h';
+        for (int i = 0; i < 5; i++) {
+            System.out.print(symb + " ");
+            symb++;
+        }
+    }
+}
